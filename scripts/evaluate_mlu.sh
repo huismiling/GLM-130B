55ba55e92c7b897b49dc810012c1620131d55d17
huismiling <zh_smiling@yeah.net>
2023-04-12 03:47:29 -0400

    add mlu eval script

---
+++ scripts/evaluate_mlu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -v 
+
+export CNCL_MEM_POOL_MULTI_CLIQUE_ENABLE=1
+
+script_path=$(realpath $0)
+script_dir=$(dirname $script_path)
+main_dir=$(dirname $script_dir)
+
+source "${main_dir}/configs/model_glm_130b_mlu.sh"
+
+DATA_PATH="data/evaluation/"
+
+ARGS="${main_dir}/evaluate.py \
+       --distributed-backend cncl \
+       --mode inference \
+       --data-path $DATA_PATH \
+       --task $* \
+       $MODEL_ARGS"
+
+TIMESTAMP=$(date +'%Y.%m.%d-%H:%M:%S')
+EXP_NAME=${TIMESTAMP}
+
+mkdir -p logs
+
+run_cmd="python -m torch.distributed.launch \
+       --nproc_per_node $MP_SIZE ${ARGS}"
+eval ${run_cmd} 2>&1 | tee logs/${EXP_NAME}.log
