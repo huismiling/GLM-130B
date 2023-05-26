28e8e99a24123b05a5d76b17d00945405cb24b0f
huismiling <zh_smiling@yeah.net>
2023-05-26 02:40:47 -0400

    support mlu quantization

---
+++ mlu/__init__.py
@@ -0,0 +1,126 @@
+import pkg_resources
+import torch
+import torch_mlu
+import ctypes
+
+from typing import List
+
+RESOURCE_PACKAGE_NAME = __name__
+
+torch.set_printoptions(precision=6, linewidth=160)
+
+class Kernel:
+    def __init__(self, filename: str, function_names: List[str]):
+        filename = filename + ".so"
+        if not pkg_resources.resource_exists(RESOURCE_PACKAGE_NAME, filename):
+            raise RuntimeError("File `%s` not found in `%s`" % (filename, RESOURCE_PACKAGE_NAME))
+        self.filename = filename
+        self.lib = ctypes.cdll.LoadLibrary(filename)
+        # self.code = pkg_resources.resource_string(RESOURCE_PACKAGE_NAME, filename)
+        self._function_names = function_names
+        # self._cmodule = LazyKernelCModule(self.code)
+
+        for name in self._function_names:
+            setattr(self, name, getattr(self.lib, name))
+
+
+kernels = Kernel(
+    "quantization",
+    [
+        "int4WeightCompression",
+        "int4WeightExtractionFloat",
+        "int4WeightExtractionHalf",
+        "int8WeightExtractionFloat",
+        "int8WeightExtractionHalf",
+    ],
+)
+
+
+def compress_int4_weight(weight: torch.Tensor):  # (n, m)
+    with torch.mlu.device(weight.device):
+        n, m = weight.size(0), weight.size(1)
+        assert m % 2 == 0
+        m = m // 2
+        out = torch.empty(n, m, dtype=torch.int8, device="mlu")
+        m = m * 2
+        stream = torch.mlu.current_stream()
+        # import pdb; pdb.set_trace()
+
+        kernels.int4WeightCompression(
+            ctypes.c_void_p(weight.data_ptr()), 
+            ctypes.c_void_p(out.data_ptr()), 
+            ctypes.c_void_p(stream.mlu_stream), 
+            ctypes.c_int32(n), 
+            ctypes.c_int32(m)
+        )
+        return out
+
+
+def extract_weight_to_half(weight: torch.Tensor, scale_list: torch.Tensor, source_bit_width: int):
+    if source_bit_width == 8:
+        func = kernels.int8WeightExtractionHalf
+    elif source_bit_width == 4:
+        func = kernels.int4WeightExtractionHalf
+    else:
+        assert False, "Unsupported bit-width"
+
+    with torch.mlu.device(weight.device):
+        n, m = weight.size(0), weight.size(1)
+        out = torch.empty(n, m * (8 // source_bit_width), dtype=torch.half, device="mlu")
+        m = out.size(1)
+        stream = torch.mlu.current_stream()
+
+        func(
+                ctypes.c_void_p(weight.data_ptr()),
+                ctypes.c_void_p(scale_list.data_ptr()),
+                ctypes.c_void_p(out.data_ptr()),
+                ctypes.c_void_p(stream.mlu_stream), 
+                ctypes.c_int32(n),
+                ctypes.c_int32(m)
+        )
+        return out
+
+
+if __name__ == "__main__":
+    torch.manual_seed(0)
+    test_shapes = [[12288, 8192], [12288, 8192], [12288, 8192], [8192, 12288]]
+    # test_shapes = [[12288, 8192]]
+
+    quantization_bit_width = 8
+
+    with torch.profiler.profile(
+            activities=[torch.profiler.ProfilerActivity.CPU,
+                torch.profiler.ProfilerActivity.MLU
+                ],
+            schedule=torch.profiler.schedule(
+                wait=1,
+                warmup=1,
+                active=7),
+            on_trace_ready=torch.profiler.tensorboard_trace_handler("profile_mlu")
+    )as p:
+
+        for ishape in test_shapes:
+            weight = torch.randn(ishape).mlu().half()
+            scale = (weight.abs().max(dim=-1).values / ((2 ** (quantization_bit_width - 1)) - 1)).half()
+            print(weight.shape)
+            print(scale.shape)
+            quant_weight = torch.round(weight / scale[:, None]).to(torch.int8)
+
+            # print(weight)
+            # b = compress_int4_weight(weight)
+            # print(b)
+
+            a = extract_weight_to_half(quant_weight, scale, source_bit_width=quantization_bit_width)
+            # print(a)
+            with torch.profiler.record_function("python"):
+                dequant_weight = quant_weight.half() * scale[:, None]
+            print(dequant_weight.shape)
+            diff = a - dequant_weight
+            p.step()
+
+            print("weight :", weight.reshape(-1)[:10])
+            print("scale :", scale.reshape(-1)[:10])
+            print("quant_weight :", quant_weight.reshape(-1)[:10])
+            print("dequant_weight :", dequant_weight.reshape(-1)[:10])
+            print("a :", a.reshape(-1)[:10])
+            print("diff :", diff.abs().sum())
---
+++ mlu/binary_op_3pipeline.h
@@ -0,0 +1,158 @@
+/*************************************************************************
+ * Copyright (C) [2022] by Cambricon, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *************************************************************************/
+#ifndef KERNELS_BINARY_OP_BINARY_OP_3PIPELINE_H_
+#define KERNELS_BINARY_OP_BINARY_OP_3PIPELINE_H_
+
+#include "kernel.h"
+#define BINARY_ALIGN_NUM 64
+
+
+#define BINARY_OP_3PIPELINE_IMPLE(Op, Dtype, Prefer)                      \
+  __mlu_global__ void MLUBlockKernel3StagePipeline##Op##Dtype##Prefer(    \
+      void *x, void *y, void *z, int32_t data_num0, int32_t data_num1,    \
+      int32_t qat_bit_width ) {                      \
+    int32_t nram_limit = 0;                                               \
+    int32_t pong_x = 0;                                                   \
+    int32_t pong_y = 0;                                                   \
+    Dtype *nram_x = NULL;                                                 \
+    Dtype *nram_y = NULL;                                                 \
+    Dtype *nram_aux1 = NULL;                                              \
+    Dtype *nram_aux2 = NULL;                                              \
+    Dtype *nram_aux3 = NULL;                                              \
+    get3OffsetDeQat##Prefer(nram_limit, pong_x, pong_y, nram_x, nram_y,    \
+                           nram_aux1, nram_aux2, nram_aux3, nram_buffer, data_num1); \
+    processBinaryPipe3<Dtype, compute##Op##Prefer>(                       \
+        (Dtype *)x, (Dtype *)y, (Dtype *)z, nram_buffer, (Dtype *)nram_x, \
+        (Dtype *)nram_y, (Dtype *)nram_aux1, (Dtype *)nram_aux2,          \
+        (Dtype *)nram_aux3, nram_limit, pong_x, pong_y, data_num0, data_num1, qat_bit_width);        \
+  }
+
+template <typename Dtype, void (*OpFunc)(Dtype *, Dtype *, Dtype *, Dtype *,
+                                         Dtype *, int32_t, int32_t)>
+__mlu_func__ void processBinaryPipe3(const Dtype *x, const Dtype *y, Dtype *z,
+                                     char *nram_buffer, Dtype *nram_x,
+                                     Dtype *nram_y, Dtype *nram_aux1,
+                                     Dtype *nram_aux2, Dtype *nram_aux3,
+                                     const int32_t nram_limit,
+                                     const int32_t pong_x, const int32_t pong_y,
+                                     const int32_t data_num0,   // n
+                                     const int32_t data_num1,    // k
+                                     const int32_t qat_bit_width
+                                     ) {
+    if (__is_mpu()) {
+        return;
+    }
+    // split data by cores
+    // Dtype just use for POWN y inDtype6_t
+    int32_t num_per_core = data_num0 / taskDim;
+    int32_t rem_for_all = data_num0 % taskDim;
+    int8_t *base_addr_x = (int8_t *)x + taskId * num_per_core * data_num1 * qat_bit_width / 8;
+    Dtype *base_addr_y = (Dtype *)y + taskId * num_per_core;
+    Dtype *base_addr_z = (Dtype *)z + taskId * num_per_core * data_num1;
+    if (rem_for_all > 0 && taskId == (taskDim - 1)) {
+        num_per_core = num_per_core + rem_for_all;
+    }
+
+    num_per_core = num_per_core * data_num1;
+    int32_t repeat = num_per_core / nram_limit;
+    int32_t rem = num_per_core % nram_limit;
+
+    int32_t x_offset = nram_limit * qat_bit_width / 8 * sizeof(int8_t);
+    int32_t y_offset = nram_limit / data_num1;
+    int32_t rem_size = rem * sizeof(Dtype);
+
+    if (repeat > 0) {
+        // L
+        __memcpy_async(nram_x, base_addr_x, x_offset, GDRAM2NRAM);
+        __memcpy_async(nram_y, base_addr_y, y_offset*sizeof(Dtype), GDRAM2NRAM);
+        __sync();
+    }
+    if (repeat > 1) {
+        // L
+        __memcpy_async(nram_x + pong_x, base_addr_x + x_offset, x_offset, GDRAM2NRAM);
+        __memcpy_async(nram_y + pong_y, base_addr_y + y_offset, y_offset*sizeof(Dtype), GDRAM2NRAM);
+        // C
+        OpFunc(nram_x, nram_y, nram_aux1, nram_aux2, nram_aux3, nram_limit, data_num1);
+        __sync();
+    }
+
+    for (int32_t i = 0; i < repeat - 2; i++) {
+        // S
+        pvLock();
+        __memcpy_async(base_addr_z + i * nram_limit, nram_x + (i % 2) * pong_x,
+                    nram_limit*sizeof(Dtype), NRAM2GDRAM);
+        pvUnlock();
+        // L
+        __memcpy_async(nram_x + (i % 2) * pong_x, base_addr_x + (i + 2) * x_offset, x_offset, GDRAM2NRAM);
+        __memcpy_async(nram_y + (i % 2) * pong_y, base_addr_y + (i + 2) * y_offset, y_offset*sizeof(Dtype), GDRAM2NRAM);
+        // C
+        OpFunc(nram_x + ((i + 1) % 2) * pong_x, nram_y + ((i + 1) % 2) * pong_y,
+            nram_aux1, nram_aux2, nram_aux3, nram_limit, data_num1);
+        __sync();
+    }
+
+    if (repeat >= 2) {
+        // S
+        pvLock();
+        __memcpy_async(base_addr_z + (repeat - 2) * nram_limit,
+                    nram_x + (repeat % 2) * pong_x, nram_limit*sizeof(Dtype),
+                    NRAM2GDRAM);
+        pvUnlock();
+    }
+    if (rem > 0) {
+        // L
+        __memcpy_async(nram_x + (repeat % 2) * pong_x,
+                    base_addr_x + repeat * x_offset, rem, GDRAM2NRAM);
+        __memcpy_async(nram_y + (repeat % 2) * pong_y,
+                    base_addr_y + repeat * y_offset, rem*sizeof(Dtype), GDRAM2NRAM);
+    }
+    if (repeat > 0) {
+        // C
+        OpFunc(nram_x + ((repeat - 1) % 2) * pong_x,
+            nram_y + ((repeat - 1) % 2) * pong_y, nram_aux1, nram_aux2,
+            nram_aux3, nram_limit, data_num1);
+    }
+    __sync();
+
+    if (repeat > 0) {
+        // S
+        pvLock();
+        __memcpy_async(base_addr_z + (repeat - 1) * nram_limit,
+                    nram_x + ((repeat - 1) % 2) * pong_x, nram_limit*sizeof(Dtype),
+                    NRAM2GDRAM);
+        pvUnlock();
+    }
+    if (rem > 0) {
+        // C
+        OpFunc(nram_x + (repeat % 2) * pong_x, nram_y + (repeat % 2) * pong_y,
+            nram_aux1, nram_aux2, nram_aux3, rem, data_num1);
+        __sync();
+        // S
+        pvLock();
+        __memcpy_async(base_addr_z + repeat * nram_limit,
+                    nram_x + (repeat % 2) * pong_x, rem_size, NRAM2GDRAM);
+        pvUnlock();
+    }
+}
+
+#endif  // KERNELS_BINARY_OP_BINARY_OP_3PIPELINE_H_
